94033850d3b50e6495bd467be9d60e96c05a9caf
Akarsh Rajput <112371948+akarshrajput@users.noreply.github.com>
2023-08-10 09:36:44 +0530

    Update structure

--- c++-for-dsa/002_Practice_Structures.cpp
+++ c++-for-dsa/002_Practice_Structures.cpp
@@ -2,20 +2,20 @@
 #include<stdio.h> // We can use c program in .cpp file using <stdio.h>.
 using namespace std;
 
-// /*Example 1:-*/
-// struct rectangle
-// {
-// int length;
-// int breadth;
-// };
-// int main()
-// {
-//     struct rectangle a;  // Declaration
-//     struct rectangle b={10,5};  // Declaration + Initialization
-//     b.length=15;  // Modify length to 15 from 10
-//     b.breadth=10;  // Modify breadth to 10 from 5
-//     cout<<"Area of Rectangle is : "<<b.length*b.breadth<<endl;  // Give me the area of rectangle
-// }
+/*Example 1:-*/
+struct rectangle
+{
+int length;
+int breadth;
+};
+int main()
+{
+    struct rectangle a;  // Declaration
+    struct rectangle b={10,5};  // Declaration + Initialization
+    b.length=15;  // Modify length to 15 from 10
+    b.breadth=10;  // Modify breadth to 10 from 5
+    cout<<"Area of Rectangle is : "<<b.length*b.breadth<<endl;  // Give me the area of rectangle
+}
 
 // /*Example 2:-*/
 // struct card  // Deck of 52 cards example
