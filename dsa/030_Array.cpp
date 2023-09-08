105c047b3f977fe20fe18891b224d13d15afbcf0
Akarsh Rajput <112371948+akarshrajput@users.noreply.github.com>
2023-09-09 00:50:15 +0530

    New Code

---
+++ dsa/030_Array.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int size;
+    // cin>>size;
+    // int arr[size];
+    // int length = sizeof(arr)/sizeof(arr[0]);
+    // for(int i=0;i<length;i++)
+    // {
+    //     cin>>arr[i];
+    // }
+
+    // // Delete in array
+    // int index;
+    // cin>>index;
+    // for(int i=index;i<length-1;i++)
+    // {
+    //     arr[i]=arr[i+1];
+    // }
+    // length--;
+
+    // Linear search
+    // int key;
+    // cin>>key;
+    // for(int i=0;i<length;i++)
+    // {
+    //     if(key==arr[i])
+    //     {
+    //         cout<<i;
+    //     }
+    // }
+
+
+    // Maximum in array
+    // int max=arr[0];
+    // for(int i=0;i<length;i++)
+    // {
+    //     if(arr[i]>max)
+    //     {
+    //         max=arr[i];
+    //     }
+    // }
+    // cout<<max;
+
+    // Minimum in array
+    // int min=arr[0];
+    // for(int i=0;i<length;i++)
+    // {
+    //     if(arr[i]<min)
+    //     {
+    //         min=arr[i];
+    //     }
+    // }
+    // cout<<min;
+
+    // Sum of array elements
+    // int sum=0;
+    // for(int i=0;i<length;i++)
+    // {
+    //     sum=sum+arr[i];
+    // }
+    // cout<<sum;
+
+    // Reverse of array
+    // int i,j;
+    // for(i=0,j=length-1;i<j;i++,j--)
+    // {
+    //     int temp=arr[i];
+    //     arr[i]=arr[j];
+    //     arr[j]=temp;
+    // }    
+
+    // Check if array is sorted
+    // for(int i=0;i<length-1;i++)
+    // {
+    //     if(arr[i]<arr[i+1])
+    //     {
+    //         cout<<"Sorted";
+    //         return 0;
+    //     }
+    //     cout<<"Not sorted";
+    //     return 0;
+    // }
+
+    // Left Shift
+    // int temp=arr[0];
+    // for(int i=0;i<length-1;i++)
+    // {
+    //     arr[i]=arr[i+1];
+    // }
+    // arr[length-1]=temp;
+
+    // Insert in array
+    // int size;
+    // cin>>size;
+    // int arr[100];
+    // int length = size;
+    // for(int i=0;i<length;i++)
+    // {
+    //     cin>>arr[i];
+    // }
+    // int pos;
+    // cin>>pos;
+    // int key;
+    // cin>>key;
+    // for(int i=length-1;i>=pos;i--)
+    // {
+    //     arr[i+1]=arr[i];
+    // }
+    // arr[pos]=key;
+    // length++;
+
+    // Sort (Bubble Sort)
+    // int i,j;
+    // for(i=0;i<length-1;i++)
+    // {
+    //     for(j=0;j<length-i-1;j++)
+    //     {
+    //         if(arr[j]>arr[j+1])
+    //         {
+    //             int temp;
+    //             temp=arr[j];
+    //             arr[j]=arr[j+1];
+    //             arr[j+1]=temp;
+    //         }
+    //     }
+    // }
+
+    // // Merging Array
+    // int size1=4;
+    // int arr1[size1]={1,2,3,4};
+    // int size2=6;
+    // int arr2[size2]={7,6,5,9,8,10};
+    // int size3=size1+size2;
+    // int arr3[size3];
+    // for (int i = 0; i < size1; i++) {
+    //     arr3[i] = arr1[i];
+    // }
+    // for (int i = 0; i < size2; i++) {
+    //     arr3[size1 + i] = arr2[i];
+    // }
+    // for (int i = 0; i < size3; i++) {
+    //     cout<<arr3[i]<<" ";
+    // }
+
+    // Display Array
+    // for(int i=0;i<length;i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+}
